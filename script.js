85a0e1724c3213fe64a67812e702e42da798743d
bradfan <bradley.tomlinson@genspark.com>
2022-02-11 13:15:49 -0700

    continuing practicing

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
+let myLeads = [];
+let oldLeads = [];
+const tabs = [
+    {
+    url: "https://www.linkedin.com/in/per-harald-borgen/"
+    }
+]
+const inputBtn = document.getElementById("input-btn");
+const saveBtn = document.getElementById("tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+function render(leads) {
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++) {
+      listItems += `<li>
+          <a target='_blank' href='${leads[i]}'>
+           ${leads[i]} 
+           </a>
+          </li>`;
+    }
+    ulEl.innerHTML = listItems;
+  }
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+saveBtn.addEventListener("click", function () {
+    
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+})
+
+deleteBtn.addEventListener("dblclick", function () {
+  myLeads = [];
+  localStorage.clear();
+  render(myLeads);
+});
+
+
+
 
